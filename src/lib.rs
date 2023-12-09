9cf759b948fe6c51dafd35fdc5a85e6938d8b606
nikolasnorth <nikolasnorth1@gmail.com>
2023-12-09 13:49:08 -0500

    Set things up

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+mod day01 {
+    pub(crate) fn part1() {
+
+    }
+
+    pub(crate) fn part2() {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day01() {
+        assert_eq!(1, day01::part1());
+        assert_eq!(1, day01::part2());
+    }
+}
